54883a7b4f3ac3ec3ec919cd0edd232e815ef985
Christopher Louie <clouie50@stuy.edu>
2025-01-08 14:23:22 -0600

    forgot to actually add main lol

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include "user.h"
+
+int main(int argc, char* argv[]){
+  if (argc == 2){
+    if (strcmp(argv[1],"Create")==0){
+      createUser();
+    }
+    else if (strcmp(argv[1],"Transaction")==0){
+      transaction();
+    }
+    else{
+      printf("~~~~~~~~~~~~~~~~~~~~\nWelcome to the bank. Enter one of the $ARGS='' below to get started:\n1.   'Create',=\n2.   'Transaction'\n~~~~~~~~~~~~~~~~~~~~\n");
+    }
+  }
+  else{
+    printf("~~~~~~~~~~~~~~~~~~~~\nWelcome to the bank. Enter one of the $ARGS='' below to get started:\n1.   'Create'\n2.   'Transaction'\n~~~~~~~~~~~~~~~~~~~~\n");
+  }
+  return 0;
+}
