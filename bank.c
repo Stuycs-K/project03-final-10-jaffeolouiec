184f505250eca5e7e58e5b3f33c092f510e7767f
yellowstoneparks <otzarj@nycstudents.net>
2025-01-10 15:01:39 -0500

    udpates

--- bank.c
+++ bank.c
@@ -19,19 +19,7 @@ void updateBankAccounts(struct Transaction transaction) {
 }
 
 // get the transaction using a pipe
-void getTransaction() {
-	int fd;
-	struct Transaction * transaction;
-	transaction = (struct transaction *) malloc(sizeof(struct Transaction));
-	
-	if (mkfifo(PIPE_NAME, 0644) == -1) {
-		perror("did not open");
-		exit(1);
-	}
-
-	printf("Created a new named pipe %s\n", PIPE_NAME);
-
-
+void getTransaction(int fd, struct Transaction * transaction) {
 	if (open(PIPE_NAME, O_RDONLY) == -1) {
 		perror("failed to open pipe");
 		exit(1);
@@ -44,10 +32,24 @@ void getTransaction() {
 	printf("Receiver: %s", transaction->receiver);
 	printf("Amount: $%d\n", transaction->amount);
 	printf("PIN: %d\n", transaction->confirmedPIN);
+
+	close(fd);
 }
 
+// in the future, for every 10 transactions, we will write to the disk; also when the file is closed
 int main() {
+	int fd; 
+  struct Transaction * transaction;
+  transaction = (struct transaction *) malloc(sizeof(struct Transaction));
+  
+  if (mkfifo(PIPE_NAME, 0644) == -1) {
+    perror("did not open");
+    exit(1);
+  }
+
+  printf("Created a new named pipe %s\n", PIPE_NAME);
+
 	while (1) {
-		getTransaction();
+		getTransaction(fd, transaction);
 	}
 }
--- bank.h
+++ bank.h
@@ -1,6 +1,6 @@
 #ifndef BANK_H
 #define BANK_H 
-void getTransaction();
+void getTransaction(int fd, struct Transaction * transaction);
 void saveEverything();
 void loadBankAccounts();
 void updateBankAccounts(struct Transaction transaction);
